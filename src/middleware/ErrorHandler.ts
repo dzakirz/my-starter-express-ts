0e823b9c8396e681862125003e738f8ee6456b79
dzakirz <dzakirz.dev@gmail.com>
2023-03-19 00:29:40 +0700

    my starter template

--- src/middleware/ErrorHandler.ts
+++ src/middleware/ErrorHandler.ts
@@ -1,5 +1,5 @@
 import { ErrorRequestHandler } from "express";
-import env from "../utils/ValidateEnv";
+import { env } from "../utils/ValidateEnv";
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
